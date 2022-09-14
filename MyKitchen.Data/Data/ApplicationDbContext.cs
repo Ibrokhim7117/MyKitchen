e1471432c22c57a2dd11c80c4b95168f4395a1eb
ibrokhim7117 <ibrohimshomurodov42@gmail.com>
2022-09-14 11:35:55 +0500

    saf

--- MyKitchen.Data/Data/ApplicationDbContext.cs
+++ MyKitchen.Data/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace MyKitchen.DataAccess.Data
 #pragma warning disable
     public class ApplicationDbContext : IdentityDbContext
     {
+        //slas
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
